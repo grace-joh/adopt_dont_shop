8c06b21681b3765ee6702373df797ea54860d4ca
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-04-01 01:14:45 -0600

    Test: add tests for application show page.

---
+++ spec/features/applications/application_show_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'application show page' do
+  before(:each) do
+    test_data
+  end
+  
+  describe 'As a visitor when I visit the application show page' do
+    it 'displays the applinaction attributes name, 
+      full address, description, name of all pets, and the status' do
+      
+      visit "/applications/#{@app1.id}"
+save_and_open_page
+      expect(page).to have_content('Name: Grace Joh')
+      expect(page).to have_content('Full Address: 1949 Bark St Minneapolis, MN 55414')
+      expect(page).to have_content('Description: I am super nice to animals.')
+      expect(page).to have_content('Name of all pets you want to adopt:')
+      expect(page).to have_content('Status: In Progress')
+    end
+  end
+
+end

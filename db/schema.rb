a6322cef900ca6df9210b0d81a02fa0361d86fed
Grace Joh <graceehjoh@gmail.com>
2023-04-01 16:25:42 -0500

    DB: add Applications owner description default

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_01_210208) do
+ActiveRecord::Schema.define(version: 2023_04_01_211307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

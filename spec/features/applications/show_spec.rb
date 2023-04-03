e9578cbda0ad97eb84c199a355d21ff300340509
Grace Joh <graceehjoh@gmail.com>
2023-04-02 19:55:58 -0500

    test: For no submit section if pets not yet added (and refactor US6 test)

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -49,31 +49,10 @@ RSpec.describe 'application show page' do
       expect(page).to have_link('Collins')
       expect(page).to have_link('Luna')
     end
-
-    describe 'User story 8' do
-      it 'I search for pets by name and I see any pet 
-        whose name PARTIALLY matches my search' do
-        fill_in 'Search pet name', with: 'Frey'
-        click_on 'Search'
-
-        expect(current_path).to eq("/applications/#{@app1.id}")
-        expect(page).to have_link('Freyja')
-      end
-    end
-
-    describe 'User story 9' do
-      it 'I search for pets by name and my search is case insensitive' do
-        fill_in 'Search pet name', with: 'collins'
-        click_on 'Search'
-
-        expect(current_path).to eq("/applications/#{@app1.id}")
-        expect(page).to have_link('Collins')
-      end
-    end
   end
 
   describe 'User story 6' do
-    it 'I see a section to submit my application' do
+    it 'I see a section to submit my application and removed add pet section' do
       expect(page).to have_content('What makes you a good owner for the pet(s) you want to adopt?')
 
       fill_in :owner_description, with: 'I have a big backyard'
@@ -81,12 +60,40 @@ RSpec.describe 'application show page' do
 
       expect(current_path).to eq("/applications/#{@app1.id}")
       expect(page).to have_content('Status: Pending')
+      expect(page).to_not have_content('Add a Pet to this Applciation')
     end
+  end
 
-    it 'doesnt have a section to submit if no pets are added' do
+  describe 'User story 7' do
+    it 'does not show (the list of pets nor) submit section if no pets added' do
       visit "/applications/#{@app5.id}"
 
+      expect(page).to_not have_content('I want to adopt:')
+
+      expect(page).to_not have_content('Submit Application')
       expect(page).to_not have_content('What makes you a good owner for the pet(s) you want to adopt?')
+      expect(page).to_not have_button('Submit Application')
+    end
+  end
+
+  describe 'User story 8' do
+    it 'I search for pets by name and I see any pet 
+      whose name PARTIALLY matches my search' do
+      fill_in 'Search pet name', with: 'Frey'
+      click_on 'Search'
+
+      expect(current_path).to eq("/applications/#{@app1.id}")
+      expect(page).to have_link('Freyja')
+    end
+  end
+
+  describe 'User story 9' do
+    it 'I search for pets by name and my search is case insensitive' do
+      fill_in 'Search pet name', with: 'collins'
+      click_on 'Search'
+
+      expect(current_path).to eq("/applications/#{@app1.id}")
+      expect(page).to have_link('Collins')
     end
   end
 end

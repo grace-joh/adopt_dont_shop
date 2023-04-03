9cd9642eb11d67848166beb4a22be68b37f526eb
Grace Joh <graceehjoh@gmail.com>
2023-04-02 19:45:23 -0500

    fix: add conditions to pet search

--- app/controllers/application_pets_controller.rb
+++ app/controllers/application_pets_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationPetsController < ApplicationController
   def create
     @application = Application.find(params[:id])
-    @pet = Pet.find(params[:pet_id])
-    ApplicationPet.create!(application_id: @application.id, pet_id: @pet.id)
+    @searched_pet = Pet.find(params[:pet_id])
+    ApplicationPet.create!(application_id: @application.id, pet_id: @searched_pet.id)
     redirect_to "/applications/#{@application.id}"
   end
 end

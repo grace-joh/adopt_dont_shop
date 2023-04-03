ce500cc2cd32ee9961cf7dc86a73898e04fa8ae9
Grace Joh <graceehjoh@gmail.com>
2023-04-02 21:24:32 -0500

    feat/test: Add shelters with pending apps to Shelter index page

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -13,4 +13,32 @@ RSpec.describe 'Admin Shelters index' do
       expect(@shelter4.name).to appear_before(@shelter2.name)
     end
   end
+
+  describe 'User Story 11' do
+    it 'states if no shelters have pending applications' do
+      expect(page).to have_content('No shelters have pending applications.')
+    end
+
+    it 'lists all shelters with a pending application' do
+      @app1 = Application.create!(name: 'Grace Joh',
+                                  street_address: '1949 Bark St',
+                                  city: 'Minneapolis',
+                                  state: 'MN',
+                                  zipcode: '55414',
+                                  home_description: 'I am super nice to animals.',
+                                  status: 'Pending')
+      @app3 = Application.create!(name: 'Alec Kapicak',
+                                  street_address: '312 Meow Ln',
+                                  city: 'Chicago',
+                                  state: 'IL',
+                                  zipcode: '55414',
+                                  home_description: 'Seriously, I really really want this animal.',
+                                  status: 'Pending')
+
+      expect(page).to have_content('Shelters with Pending Applications')
+      expect(page).to have_content(@shelter1.name)
+      expect(page).to have_content(@shelter3.name)
+      expect(page).to have_content(@shelter4.name)
+    end
+  end
 end

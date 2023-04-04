19f441d9e9c5a4cfb4fa7adc7620003189b419bc
Grace Joh <graceehjoh@gmail.com>
2023-04-04 17:51:07 -0500

    refactor: implement enum methods

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -6,11 +6,11 @@ class Admin::ApplicationsController < ApplicationController
 
   def update
     pet_id = params[:approved] || params[:rejected]
-    app_pet = ApplicationPet.where('pet_id = ? and application_id = ?', pet_id, params[:id])
+    app_pet = ApplicationPet.find_by('pet_id = ? and application_id = ?', pet_id, params[:id])
     if params[:approved]
-      app_pet.update(pet_status: 'approved')
+      app_pet.approved!
     elsif params[:rejected]
-      app_pet.update(pet_status: 'rejected')
+      app_pet.rejected!
     end
     redirect_to "/admin/applications/#{params[:id]}"
   end

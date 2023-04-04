fb94efe239edbe7fb06c28d2ff4c95c9dc750ada
Grace Joh <graceehjoh@gmail.com>
2023-04-04 13:17:12 -0500

    feat: Admin Application show page reject button

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -2,13 +2,16 @@ class Admin::ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
     @app_pets = @application.pets
-    
   end
 
   def update
-    app_pet = ApplicationPet.where("pet_id = ? and application_id = ?", params[:pet_id], params[:id])
-    app_pet.update(pet_status: "approved")
+    pet_id = params[:approved] || params[:rejected]
+    app_pet = ApplicationPet.where('pet_id = ? and application_id = ?', pet_id, params[:id])
+    if params[:approved]
+      app_pet.update(pet_status: 'approved')
+    elsif params[:rejected]
+      app_pet.update(pet_status: 'rejected')
+    end
     redirect_to "/admin/applications/#{params[:id]}"
-    
   end
 end
